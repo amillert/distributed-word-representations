3dd42399c655ca609e2ee9cc7ad2728b49add80f
devmood <amillert@roq.ad>
2019-09-19 04:02:13 +0200

    Some updates

---
+++ devmood/nn/learning/nn_learner.py
@@ -0,0 +1,81 @@
+from devmood.nn.custom.tadziu_dataset import TadziuDataset
+from devmood.nn.models.word2vec import SkipGram
+
+import os
+import json
+import time
+from datetime import datetime
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+
+def learn(args):
+    dataset = TadziuDataset(args)
+    print(f"amount of data: {len(dataset)}")
+    batches = DataLoader(dataset=dataset, batch_size=args["batch_size"], shuffle=True, num_workers=6)
+
+    vocab_size = dataset.vocab_size
+    FAKE = dataset.FAKE
+
+    criterion = nn.NLLLoss(ignore_index=FAKE)
+
+    model = SkipGram(vocab_size, args["dims"], FAKE)
+    optimizer = torch.optim.Adam(model.parameters(), lr=args["eta"])
+
+    num_batches = len(dataset) // args["batch_size"]
+    print(f"number of upcoming batches: {num_batches}")
+
+    for epoch in range(args["epochs"]):
+        tick = time.time()
+        loss_total = 0.0
+        batch_count = 0
+        for target, context in batches:
+            batch_count += 1
+            loss = criterion(model(context), target)
+            loss_total += loss.item()
+
+            if not batch_count % 10 and batch_count > 1:
+                print(f"loss of batch {batch_count} during epoch {epoch + 1} is: {loss.item() / batch_count}")
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        print("~~~~~~~~~~~~~~~~~~")
+        print(f"end of epoch: {epoch + 1} out of {args['epochs']}")
+        print(f"time per epoch: {time.time() - tick}")
+        print(f"mean loss: {loss_total / num_batches}")
+        print(f"total loss: {loss_total}")
+        print("~~~~~~~~~~~~~~~~~~")
+
+        weights_in = model.embedding_layer_in.weight.detach().numpy()
+        # weights_out = model.embedding_layer_out.weight.detach().numpy()
+        # weights = (weights_in + weights_out) / 2
+        words = dataset.vocab
+
+    save_weights(dataset, weights_in, words, epoch + 1)
+    save_results(args, model.__class__.__name__, loss_total, loss_total / num_batches)
+
+
+def save_weights(dataset, weights, words, epoch):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+              f"../results/weights/{datetime.now()}.csv"), "w") as fout:
+        for word in words:
+            vec = weights[dataset.word2idx[word]].reshape(1, -1)[0]
+            fout.write(f"{word},{' '.join(vec.astype(str).tolist())}\n")
+
+def save_results(args, model_name, loss, mean_loss):
+    with open(os.path.join(os.path.dirname(os.path.abspath(__file__)),
+              f"../results/{model_name}-results.json"), "a") as fout:
+        fout.write(json.dumps({
+            "window": args["window"],
+            "batch": args["batch_size"],
+            "dims": args["dims"],
+            "eta": args["eta"],
+            "epochs": args["epochs"],
+            "file": os.path.basename(args["input"]),
+            "total loss": loss,
+            "mean loss": mean_loss
+        }, indent=2) + ",\n")
+
--- xd.py
+++ xd.py
@@ -1,71 +1,145 @@
 import re
 import os
+import copy
+import time
+import numpy as np
 from smart_open import smart_open as open
 from pprint import pprint
 from collections import Counter
 
+from torch.utils.data import Dataset
 
-MAIN_DIR = os.getcwd()
-ELLIPSIS = chr(8230)
-QUOTE = chr(187)
-MINUS = chr(8212)
-text = open(os.path.join(MAIN_DIR, "data/pan-tadziu.txt"), encoding="utf-8").read().replace(ELLIPSIS, ".").replace(QUOTE, "").replace(MINUS, "")
-pol_stop_words = open(os.path.join(os.getcwd(), "data/polish-stopwords.txt"), encoding="utf-8").read().split("\n")
+from devmood.nn.learning.nn_learner import learn
 
-REGEX_DOC_SPLIT = "(Księga)[a-z ąćęłóźż]*[\n]{4,}"
-DOC_SPLITTER = "`~`"
-docs = [' '.join(' '.join(x.split("\n\n")[1:]).split("    ")[1:]).replace("\n", " ") for x in re.sub(REGEX_DOC_SPLIT, DOC_SPLITTER, text).split(DOC_SPLITTER) if x != ""]
 
-docs2sentences = [re.sub("[.!?]", ".", doc).strip().split(".") for doc in docs]
-docs2sentences = [[re.sub("[,;:*(){}\-\/]", "", sentence.strip()) for sentence in doc if sentence != ""] for doc in docs2sentences]
+# MAIN_DIR = os.getcwd()
+# ELLIPSIS = chr(8230)
+# QUOTE = chr(187)
+# MINUS = chr(8212)
+# text = open(os.path.join(MAIN_DIR, "data/pan-tadziu.txt"), encoding="utf-8").read().replace(ELLIPSIS, ".").replace(QUOTE, "").replace(MINUS, "")
+# pol_stop_words = open(os.path.join(os.getcwd(), "data/polish-stopwords.txt"), encoding="utf-8").read().split("\n")
+# 
+# REGEX_DOC_SPLIT = "(Księga)[a-z ąćęłóźż]*[\n]{4,}"
+# DOC_SPLITTER = "`~`"
+# docs = [' '.join(' '.join(x.split("\n\n")[1:]).split("    ")[1:]).replace("\n", " ") for x in re.sub(REGEX_DOC_SPLIT, DOC_SPLITTER, text).split(DOC_SPLITTER) if x != ""]
+# 
+# docs2sentences = [re.sub("[.!?]", ".", doc).strip().split(".") for doc in docs]
+# docs2sentences = [[re.sub("[,;:*(){}\-\/]", "", sentence.strip()) for sentence in doc if sentence != ""] for doc in docs2sentences]
+# docs2sentences2tokens = [[[word.lower() for word in sentence.split() if word.lower() not in pol_stop_words] for sentence in doc] for doc in docs2sentences]
+# 
+# fake = -1
+# window = 4
+# thresh = 1
+#  
+# def invert_dict(dic):
+#   return {v: k for k, v in dic.items()}
+# 
+# def get_all_stuff(docs2sentences2tokens):
+#   tokens = [word for doc in docs2sentences2tokens for sentence in doc for word in sentence]
+#   print(f"Amount of tokens: {len(tokens)}")
+#   vocab = sorted(list(set(tokens)))
+#   print(f"Vocab size: {len(vocab)}")
+#   word2idx = {word: idx for idx, word in enumerate(vocab)}
+#   idx2word = invert_dict(word2idx)
+#   
+#   all_tcp = []
+#   for doc in docs2sentences2tokens:
+#     for sentence in doc:
+#       for center_idx in range(len(sentence)):
+#         for context in range(-window // 2, window // 2 + 1):
+#           context_idx = center_idx + context
+#           if context_idx < 0 or context_idx >= len(sentence):
+#             all_tcp.append(tuple([center_idx, fake]))
+#           elif context_idx != center_idx:
+#             all_tcp.append(tuple([word2idx[sentence[center_idx]], word2idx[sentence[context_idx]]]))
+#   
+#   all_tcp_strings = [f"{pair[0]} {pair[1]}" for pair in all_tcp]
+#   print(f"Len of all tcps: {len(all_tcp)}")
+# 
+#   pair2count = Counter(all_tcp_strings)
+#   # pprint(sorted(pair2count.items(), key=lambda l: -l[1])[:10])
+# 
+#   tcp_strings_filtered = [tcp for tcp in all_tcp_strings if pair2count[tcp] > thresh]
+#   print(f"Len of tcp strings filtered: {len(tcp_strings_filtered)}")
+#   
+#   tcp_idx = [[int(x[0]), int(x[1])] for x in [pair.split(" ") for pair in tcp_strings_filtered]]
+#   print(f"Len of tcp idx filtered: {len(tcp_idx)}")
+#   vocab_idx = sorted(list(set([idx for x in tcp_idx for idx in x if idx != fake])))
+#   print(f"New vocab filtered size: {len(vocab_idx)}")
+#   word2idx = {idx2word[idx]: idx for idx in vocab_idx}
+# 
+#   return word2idx, tcp_idx, vocab_idx, pair2count
+# 
+# word2idx, tcp_idx, vocab_idx, pair2count = get_all_stuff(docs2sentences2tokens)
+# idx2word = invert_dict(word2idx)
+# 
+# inner = {word_idx: 0 for word_idx in vocab_idx}
+# alls = {word_idx: copy.deepcopy(inner) for word_idx in vocab_idx}
+# 
+# # TODO: make suer the keys are alway in good order; think about how to check if values are good (in pairs should have each other?)
+# # matrix_np = np.zeros((len(vocab_idx), len(vocab_idx)))
+# 
+# row2idx = {row: idx for idx, row in zip(vocab_idx, range(len(vocab_idx)))}
+# idx2row = invert_dict(row2idx)
+# 
+# for target, context in tcp_idx:
+#   if target != fake != context:
+#     if pair2count[f"{target} {context}"] > 0:
+#       alls[target][context] = pair2count[f"{target} {context}"]
+# 
+# path = "/home/amillert/private/distributed-word-representations/data/tmp.npy"
+# matrix_np = np.array([list(alls[idx].values()) for idx in vocab_idx])
+# np.save(path, matrix_np)
+# matrix_np = np.load(path)
+# 
+# Preview of the matrix:
+# for i, x in enumerate(matrix_np):
+#   print(f"Values found for the word {idx2word[row2idx[i]]} are {' '.join(set([str(xi) for xi in x]))}")
 
-def get_all_stuff(docs2sentences):
-  docs2sentences2tokens = [[[word.lower() for word in sentence.split()] for sentence in doc] for doc in docs2sentences]
-  tokens = [word for doc in docs2sentences2tokens for sentence in doc for word in sentence]
-  print(f"Amount of tokens: {len(tokens)}")
-  vocab = sorted(list(set(tokens)))
-  print(f"Vocab size: {len(vocab)}")
-  word2idx = {word: idx for idx, word in enumerate(vocab)}
-  idx2word = {idx: word for word, idx in word2idx.items()}
-  
-  fake = -1
-  window = 4
-  
-  all_tcp = []
-  for doc in docs2sentences2tokens:
-    doc_tcp = []
-    for sentence in doc:
-      tcp = []
-      for center_idx in range(len(sentence)):
-        for context in range(-window // 2, window // 2 + 1):
-          context_idx = center_idx + context
-          if context_idx < 0 or context_idx >= len(sentence):
-            tcp.append(tuple([center_idx, fake]))
-          elif context_idx != center_idx:
-            tcp.append(tuple([word2idx[sentence[center_idx]], word2idx[sentence[context_idx]]]))
-        doc_tcp.append(tcp)
-    all_tcp.append(doc_tcp)
-  
-  print(f"Len of all tcps: {len(all_tcp)}")
-  
-  all_tcp_strings = [f"{pair[0]} {pair[1]}" for doc_tcp in all_tcp for tcp in doc_tcp for pair in tcp]
-  pair2count = Counter(all_tcp_strings)
-  word2count = Counter(tokens)
-  
-  print(f"Len of pair2counts: {len(pair2count.keys())}")
-  print(f"Len of word2ounts: {len(word2count.keys())}")
-  pprint(sorted(pair2count.items(), key=lambda l: -l[1])[:10])
-  pprint(sorted(word2count.items(), key=lambda l: -l[1])[:10])
-  return word2idx, idx2word, all_tcp, all_tcp_strings, pair2count, word2count, vocab
+def vector_length(v):
+  return np.sqrt(np.dot(v, v))
 
-word2idx, idx2word, all_tcp, all_tcp_strings, pair2count, word2count, vocab = get_all_stuff(docs2sentences)
+def euclidean_distance(u, v):
+  return vector_length(u - v)
 
-docs2sentences = [[' '.join([word.lower() for word in sentence.split() if word.lower() not in pol_stop_words]) for sentence in doc] for doc in docs2sentences]
+def normalized_length(v):
+  return v / vector_length(v)
 
-print("")
-print("")
-print("")
+def cosine_distance(u, v):
+  return 1.0 - (np.dot(u, v) / (vector_length(u) * vector_length(v)))
 
-word2idx, idx2word, all_tcp, all_tcp_strings, pair2count, word2count, vocab = get_all_stuff(docs2sentences)
+def intersection(u, v):
+  return np.sum(np.minimum(u, v))
 
+def union(u, v):
+  return np.sum(np.maximum(u, v))
+
+def jaccard_distance(u, v):
+  return 1.0 - (intersection(u, v) / union(u, v))
+
+def jensen_shannon_distance(u, v):
+  return np.sum(u * np.log(u / v)) ** 2.0
+
+def dice_coefficient(u, v):
+  return 1.0 - (2.0 * np.sum(np.minimum(u, v)) / np.sum(u + v))
+
+triangle = np.array([[ 2.0,  4.0], [10.0, 15.0], [14.0, 10.0]])
+
+# think of counting the area of the triangles given those metrics
+# for distance_metric in (euclidean_distance, cosine_distance, jaccard_distance, jensen_shannon_distance, dice_coefficient):
+#   #format = {"name": distance_metric.__name__,  "AB": distance_metric(triangle[0], triangle[1]), "BC": distance_metric(triangle[1], triangle[2])}
+#   print(f"{distance_metric.__name__}")
+#   print(f"|AB| = {distance_metric(triangle[0], triangle[1])}")
+#   print(f"|BC| = {distance_metric(triangle[1], triangle[2])}")
+#   print(f"|AC| = {distance_metric(triangle[0], triangle[2])}", end="\n")
+
+# def neighbours(word, matrix, rownames, distance=euclidean_distance):
+#   palabra = matrix[rownames.index(word)]
+#   distances = [(rownames[i], distance(palabra, matrix[i])) for i in xrange(len(mat))]
+#   return sorted(distances, key=lambda l: l[1])
+#   # return sorted(distances, key=itemgetter(1), reverse=False)
+# 
+# neighbours("ksiądz", matrix_np, rownames=ww[1])
+args = {"batch_size": 128, "dims": 50, "eta": 0.01, "epochs": 100, "input": "Pan Tadeusz"}
+learn(args)
 
